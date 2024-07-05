f9c4afc087fdec484fe139f8083a8650fb3cec4c
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2024-07-05 15:30:21 +0300

    static

---
+++ classes/les-48.js
@@ -0,0 +1,9 @@
+var UserServices = /** @class */ (function () {
+    function UserServices() {
+    }
+    return UserServices;
+}());
+(function () {
+    UserServices.db = 'dskjfkjsd';
+})();
+console.log(UserServices.db);
---
+++ classes/les-48.ts
@@ -0,0 +1,14 @@
+class UserServices {
+  static db: any;
+
+  static async getUser(id: number) {
+    await fetch("");
+  }
+
+  static {
+    UserServices.db = "dskjfkjsd";
+    UserServices.getUser(1)
+  }
+}
+
+console.log(UserServices.db);
